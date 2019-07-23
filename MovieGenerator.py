6f8853c8eb514415e9950abeb4e4a37d533682ce
bzahid <becky.zahid@bridgepaynetwork.com>
2019-07-23 16:42:35 -0400

    code cleanup

---
+++ MovieGenerator.py
@@ -0,0 +1,20 @@
+import random
+
+def again(res):
+	res = res.lower()
+	return res == "yes" or res == "y" or res == "Y"
+
+min = 0
+max = 101
+res = "y"
+
+while again(res):
+    
+	num = random.randint(min, max)
+	
+	with open("movies.txt") as file:
+		for i, line in enumerate(file):
+			if i == num:
+				print("\nJack has not seen: " + line.strip());
+	
+	res = raw_input("Continue? ")
